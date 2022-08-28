3fc90d6985e582deab1480955b31879da085cd45
Adan <adanaligakhar@gmail.com>
2022-08-28 14:30:05 +0500

    setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Recipe,Home,Create,Search} from './pages';
+import { Routes,Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-     App
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/create' element={<Create/>} />
+      <Route path='/search' element={<Search/>} />
+      <Route path='/recipes/:id' element={<Recipe/>} />
+     </Routes>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import  Home  from "./Home";
+import Create from "./Create";
+import Recipe from "./Recipe";
+import Search from "./Search";
+
+
+export  {Home,Create,Recipe,Search};
